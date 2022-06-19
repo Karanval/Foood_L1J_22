3866141c7e8b43aa1f87541d584905cee2ca258d
Ronny <ronny.mikkelsen@gmail.com>
2022-06-19 15:16:10 +0200

    SoundManager + HappyFoodSounds

--- Scripts/SoundManager.gd
+++ Scripts/SoundManager.gd
@@ -1,33 +1,57 @@
 extends Node2D
 
 
-var sounds1 = [
+var Block1 = [
 	preload("res://Resources/Sounds/Music/Block/Block_00.ogg"),
 	preload("res://Resources/Sounds/Music/Block/Block_01.ogg"),
 	preload("res://Resources/Sounds/Music/Block/Block_10.ogg")]
 	
-var sounds2 = [
+var Block2 = [
 	preload("res://Resources/Sounds/Music/Block/Block_11.ogg"),
 	preload("res://Resources/Sounds/Music/Block/Block_20.ogg"),
 	preload("res://Resources/Sounds/Music/Block/Block_21.ogg")]
 	
+var HappyFood = [
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_00.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_01.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_02.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_03.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_04.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_05.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_06.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_07.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_08.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_09.ogg"),
+	preload("res://Resources/Sounds/Dx/HappyFood/Dx_HappyFood_10.ogg")
+	]
 
-func _on_AudioStreamPlayer_finished():
-	$AudioStreamPlayer.stop()
-	$AudioStreamPlayer2.stop()
+
+func _on_Block_1_finished():
+	$Block_1.stop()
+	$Block_2.stop()
 	
 	var random = RandomNumberGenerator.new()
 	random.randomize()
 	var index = random.randi_range(0, 2)
-	var sound1 = sounds1[index]
+	var blocksound1 = Block1[index]
 	index = random.randi_range(0, 2)
-	var sound2 = sounds2[index]
+	var blocksound2 = Block2[index]
 	
-	$AudioStreamPlayer.stream = sound1
-	$AudioStreamPlayer2.stream = sound2
+	$Block_1.stream = blocksound1
+	$Block_2.stream = blocksound2
 	
-	$AudioStreamPlayer.play()
-	$AudioStreamPlayer2.play()
+	$Block_1.play()
+	$Block_2.play()
+	
+func PlayHappySound():
+	var random = RandomNumberGenerator.new()
+	random.randomize()
+	var index = random.randi_range(0, 10)
+	var happysound = HappyFood[index]
+	
+	$HappyFood.stream = happysound
+	$HappyFood.play()
+	
 	
 
 
